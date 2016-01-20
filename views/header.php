4fe46d802fd57d0d3382da78a784326f0167823f
Timendum <timedum@gmail.com>
2016-01-20 22:31:15 +0100

    fix for login/out

--- views/header.php
+++ views/header.php
@@ -21,13 +21,11 @@
 					<li><a href="./">Download</a></li>
 					<li><a href="./list.php">List of files</a></li>
 					<?php
-						if ($file->is_log_enabled()) {
-							echo '<li><a href="./logs.php">Logs</a></li>';
-						}
-					?>
-					<?php
 						if($session->is_logged_in())
 						{
+							if (isset($file) && $file->is_log_enabled()) {
+								echo '<li><a href="./logs.php">Logs</a></li>';
+							}
 					?>
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
