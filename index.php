85be8dcd2bfed9b0c4489cde53d7e8c9b6287041
Timendum <timedum@gmail.com>
2016-02-24 09:50:20 +0100

    typo

--- index.php
+++ index.php
@@ -118,7 +118,7 @@
 							<p><b>How can I download the video on my computer?</b></p>
 							<p>Go to <a href="./list.php?type=v">List of videos</a> -> choose one -> right click on the link -> "Save target as ..." </p>
 							<p><b>What's Filename or Format field?</b></p>
-							<p>They are optional, see the official documentation about <a href=".https://github.com/rg3/youtube-dl/blob/master/README.md#format-selection">Format selection</a> or <a href="https://github.com/rg3/youtube-dl/blob/master/README.md#output-template">Output template</a> </p>
+							<p>They are optional, see the official documentation about <a href="https://github.com/rg3/youtube-dl/blob/master/README.md#format-selection">Format selection</a> or <a href="https://github.com/rg3/youtube-dl/blob/master/README.md#output-template">Output template</a> </p>
 						</div>
 					</div>
 				</div>
