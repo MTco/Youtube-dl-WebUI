a66aec4f38e6c8a5ca318db4a07f79a4b1dbb22c
Timendum <timedum@gmail.com>
2022-05-24 11:58:16 +0200

    Update header.php

--- views/header.php
+++ views/header.php
@@ -3,8 +3,8 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Youtube-dl WebUI</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" media="screen">
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.1/css/fontawesome.min.css" media="screen">
+		<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.1/css/fontawesome.min.css">
 		<link rel="stylesheet" href="./css/custom.css">
 		<link rel="Shortcut Icon" href="./favicon_144.png" type="image/x-icon">
 	</head>
