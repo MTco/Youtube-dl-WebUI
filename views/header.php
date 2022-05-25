befdb8ce91cda4c312fe8c49b3af90f060696f67
Timendum <timedum@gmail.com>
2022-05-25 11:39:17 +0200

    Update header.php

--- views/header.php
+++ views/header.php
@@ -19,26 +19,45 @@
 					<?php
 						if($session->is_logged_in() && isset($file))
 					{
-						echo '						<li class="nav-item"><a class="nav-link" href="./"><span class="glyphicon glyphicon-cloud-download"></span> Download</a></li>';
-						echo '						<li class="nav-item"><a class="nav-link" href="./info.php"><span class="glyphicon glyphicon-info-sign"></span> JSON Info</a></li>';
+						?>
+						<li class="nav-item"><a class="nav-link" href="./">
+							<span class="align-text-bottom"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-download" viewBox="0 0 16 16"><path d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z"/><path d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"/></svg></span>
+							Download
+						</a></li>
+						<li class="nav-item"><a class="nav-link" href="./info.php">							
+							<span class="align-text-bottom"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle-fill" viewBox="0 0 16 16">  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/></svg></span>
+							JSON Info
+						</a></li>
+						<li class="nav-item"><a class="nav-link" href="./list.php">
+							<span class="align-text-bottom"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/></svg></span>
+						<?php
 						// List of files
 						$filesCount = count($file->listFiles());
 						if ($filesCount < 1) {
-							echo '					<li class="nav-item"><a class="nav-link" href="./list.php"><span class="glyphicon glyphicon-list"></span> List of files</a></li>';
+							echo '							List of files';
 						} else {
-							echo '					<li class="nav-item"><a class="nav-link" href="./list.php"><span class="glyphicon glyphicon-list"></span> <b>List of files</b> ('.($filesCount).')</a></li>';
+							echo '							<b>List of files</b> ('.($filesCount).')';
 						}
 						unset($filesCount);
-						
+						?>
+						</a></li>
+						<?php
 						// Logs
 						if ($file->is_log_enabled()) {
+							?>
+						<li class="nav-item"><a class="nav-link" href="./logs.php">
+							<span class="align-text-bottom"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-journals" viewBox="0 0 16 16"><path d="M5 0h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2 2 2 0 0 1-2 2H3a2 2 0 0 1-2-2h1a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1H1a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v9a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1H3a2 2 0 0 1 2-2z"/><path d="M1 6v-.5a.5.5 0 0 1 1 0V6h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V9h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 2.5v.5H.5a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1H2v-.5a.5.5 0 0 0-1 0z"/></svg>
+						<?php
 							$filesCount = $file->countLogs();
 							if ($filesCount < 1) {
-								echo '						<li class="nav-item"><a class="nav-link" href="./logs.php"><span class="glyphicon glyphicon-book"></span> Logs</a></li>';
+								echo '						Logs';
 							} else {
-								echo '						<li class="nav-item"><a class="nav-link" href="./logs.php"><span class="glyphicon glyphicon-book"></span> <b>Logs</b> ('.($filesCount).')</a></li>';
+								echo '						<b>Logs</b> ('.($filesCount).')';
 							}
 							unset($filesCount);
+							?>
+						</a></li>
+							<?php
 						}
 					?>
 						<li class="nav-item dropdown">
@@ -73,7 +92,12 @@
 					<?php
 						if($session->is_logged_in())
 						{
-							echo '					<li class="nav-item"><a class="nav-link" href="./logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>';
+							?>
+					<li class="nav-item"><a class="nav-link" href="./logout.php">
+						<span class="align-text-bottom"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/><path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/></svg></span>
+						Logout
+					</a></li>
+							<?php
 						}
 					?>
 				</ul>
