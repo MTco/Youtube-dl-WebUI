318f40243b92bba6921e27f8a0cd2b0adcdbd8a3
Timendum <timedum@gmail.com>
2022-05-25 11:55:25 +0200

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" crossorigin="anonymous"></script>
 
         <script type="text/javascript">
                 document.addEventListener("DOMContentLoaded", function() {
